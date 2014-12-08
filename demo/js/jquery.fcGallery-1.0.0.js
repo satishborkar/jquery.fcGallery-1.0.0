dcce426be37357c2698597d36a4ac5ef46dd8bbe
Satish Prakash Borkar <satyaspb@gmail.com>
2014-12-08 19:50:15 +0530

    jQuery full screen Gallery
    jQuery full screen Gallery by Satish Borkar

---
+++ demo/js/jquery.fcGallery-1.0.0.js
@@ -0,0 +1,71 @@
+﻿$(function ($) {
+    $.fn.fullScreenGallery = function () {
+        this.each(function () {
+            $(this).prepend("<div id='gall-nav'><a href='#previous'> < </a><a href='#next'> >  </a></div><ul class='item-list'></ul>");
+           
+            var elem = $(this).find('img');
+            var listContainer = $('.item-list');
+            var elemSrc = 'undefined'
+            
+            $(elem).each(function () {
+                elemSrc = $(this).attr('src');
+                listContainer.append('<li style="background-image:url(' + elemSrc + ')"> &nbsp; </li>');
+            });
+
+            elem.attr('disabled', 'disabled').css({ 'display': 'none' });
+
+
+            var items = $(this).find('li');
+            var prevLink = $('#gall-nav a[href="#previous"]');
+            var nextLink = $('#gall-nav a[href="#next"]');
+            var current = items.eq(0);
+            var autoPlay = 'undefined';
+
+            autoLoad();
+
+            function jumpTo(position) {
+                items.eq(parseInt(position)).fadeIn().addClass('active');
+            }
+
+            function deactivate() {
+                current.removeClass('active');
+                current = listContainer.find('li.active');
+            }
+
+            function hideAll() {
+                items.hide();
+            }
+
+            function nextSlide() {
+                hideAll();
+                (current.next().length != 0) ? current.next().eq(0).fadeIn().addClass('active') : jumpTo(0);
+                deactivate();
+            }
+
+            function prevSlide() {
+                hideAll();
+                (current.prev().length != 0) ? current.prev().eq(0).addClass('active').fadeIn() : jumpTo(-1);
+                deactivate();
+            }
+
+            hideAll();
+            jumpTo(0);
+            nextLink.click(function () {
+                nextSlide();
+                autoLoad();
+            });
+
+            prevLink.click(function () {
+                prevSlide();
+                autoLoad();
+            });
+
+            function autoLoad() {
+                clearInterval(autoPlay);
+                autoPlay = setInterval(function () { nextSlide(); }, 7000);
+            }
+
+
+        })
+    }
+}(jQuery));
---
+++ jquery.fcGallery-1.0.0.js
@@ -0,0 +1,80 @@
+﻿/**
+ * jQuery.fcGallery - Fullscreen Slider .
+ * Copyright (c) 2014-2015 | http://satishborkar.wordpress.com/ and http://manojkulkarni2006.wordpress.com/
+ * Dual licensed under MIT and GPL.
+ * Date: 26/09/2014
+ * @author Manoj Kukarni & Satish Borkar
+ * @version 1.0.0
+ **/
+
+/**
+Example :
+<div class="gallery">
+Keep your all images under this div
+    <img src="img/01.jpg" alt="" />
+    <img src="img/02.jpg" alt="" />
+</div>   
+
+$('.gallery').fcGallery();
+
+***/
+
+$(function (a) {
+    a.fn.fcGallery = function () {
+        this.each(function () {
+            a(this).prepend("<div id='gall-nav'><a href='#previous'> < </a><a href='#next'> >  </a></div><ul class='item-list'></ul>");
+            var b = a(this).find("img");
+            var c = a(".item-list");
+            var d = "undefined";
+            a(b).each(function () {
+                d = a(this).attr("src");
+                c.append('<li style="background:url(' + d + ')center center fixed no-repeat"> &nbsp; </li>');
+            });
+            b.attr("disabled", "disabled").css({
+                display: "none"
+            });
+            var e = a(this).find("li");
+            var f = a('#gall-nav a[href="#previous"]');
+            var g = a('#gall-nav a[href="#next"]');
+            var h = e.eq(0);
+            var i = "undefined";
+            o();
+            function j(a) {
+                e.eq(parseInt(a)).fadeIn().addClass("active");
+            }
+            function k() {
+                h.removeClass("active");
+                h = c.find("li.active");
+            }
+            function l() {
+                e.hide();
+            }
+            function m() {
+                l();
+                0 != h.next().length ? h.next().eq(0).fadeIn().addClass("active") : j(0);
+                k();
+            }
+            function n() {
+                l();
+                0 != h.prev().length ? h.prev().eq(0).addClass("active").fadeIn() : j(-1);
+                k();
+            }
+            l();
+            j(0);
+            g.click(function () {
+                m();
+                o();
+            });
+            f.click(function () {
+                n();
+                o();
+            });
+            function o() {
+                clearInterval(i);
+                i = setInterval(function () {
+                    m();
+                }, 7e3);
+            }
+        });
+    };
+}(jQuery));
