1adc3a4d04679568abca7c89ca4111baa71a7a29
Satish Prakash Borkar <satyaspb@gmail.com>
2015-01-20 11:27:02 +0530

    Update jquery.fcGallery-1.0.0.js

--- jquery.fcGallery-1.0.0.js
+++ jquery.fcGallery-1.0.0.js
@@ -1,3 +1,23 @@
+/**
+* jQuery.fcGallery - Fullscreen Slider .
+* Copyright (c) 2014-2015 | http://satishborkar.wordpress.com/ and http://manojkulkarni2006.wordpress.com/
+* Dual licensed under MIT and GPL.
+* Date: 26/09/2014
+* @author : Satish Borkar & Manoj Kukarni
+* @version : 1.0.0
+**/
+/**
+Example :
+<div class="fc-gallery">
+Keep your all images under this div
+    <img src="img/01.jpg" alt="" />
+    <img src="img/02.jpg" alt="" />
+</div>
+
+$('.fc-gallery').fullHeight();
+$('.fc-gallery').fullScreenGallery();
+
+***/
 $(function ($) {
     $.fn.fullScreenGallery = function () {
         $(this).each(function () {
